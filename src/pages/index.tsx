64586195f20c9b4e4465de081dacc7d6ea3c77db
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 21:32:31 -0300

    💥 Dynamic port selection implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import Door from "../Components/Door/Door";
+import { createDoors, updatePorts } from "../functions/doors";
 import DoorModel from "../model/door";
 
-
 export default function Home() {
-  const [p1, setP1] = useState(new DoorModel(1))
+  console.log(createDoors(3, 2));
 
-  return (
-    <div style={{display: "flex"}}>
-      <Door value={p1} onChange={newDoor => setP1(newDoor)}/>
-    </div>
-  )
+  const [doors, setDoors] = useState(createDoors(4, 2));
+
+  function renderDoors() {
+    return doors.map((door) => {
+      return (
+        <Door
+          value={door}
+          key={door.number}
+          onChange={(newDoor) => setDoors(updatePorts(doors, newDoor))}
+        />
+      );
+    });
+  }
+
+  return <div style={{ display: "flex" }}>{renderDoors()}</div>;
 }
