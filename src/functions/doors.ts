d0c7c1164cb79db2625b4b4698404f5a42ce6d7e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 21:31:42 -0300

    ✨ Port selection configured

---
+++ src/functions/doors.ts
@@ -0,0 +1,23 @@
+import DoorModel from "../model/door";
+
+export function createDoors(qtd: number, selected: number): DoorModel[] {
+  return Array.from({ length: qtd }, (_, i) => {
+    const number = i + 1;
+    const haveThis = number === selected;
+    return new DoorModel(number, haveThis);
+  });
+}
+
+export function updatePorts(
+  doors: DoorModel[],
+  modifiedDoor: DoorModel
+): DoorModel[] {
+  return doors.map((door) => {
+    const modifiedEqual = door.number === modifiedDoor.number;
+    if (modifiedEqual) {
+      return modifiedDoor;
+    } else {
+      return modifiedDoor.doorOpen ? door : door.deselect();
+    }
+  });
+}
