08db14a496fcd861a63d4d0122686b4200ee4c94
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-04 20:58:12 -0300

    💥 Implementation of the Card component in the index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import Card from "../Components/Card/Card";
+import styles from "../styles/Form.module.css";
+import Link from "next/link"
+
+export default function Form() {
   return (
-    <div>
-      <h1>Beginning of the game</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Card bgColor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div>
+        <Card></Card>
+        <Card bgColor="#28a085">
+          <Link href={`/game/4/2`} passHref>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
