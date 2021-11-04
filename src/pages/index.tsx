40531b302ade9c1fa96fe27917a8e6c3dbfa65c5
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-03 23:27:57 -0300

    🔥 There were changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import Door from "../Components/Door/Door";
-import { createDoors, updatePorts } from "../functions/doors";
-import DoorModel from "../model/door";
-
 export default function Home() {
-  console.log(createDoors(3, 2));
-
-  const [doors, setDoors] = useState(createDoors(4, 2));
-
-  function renderDoors() {
-    return doors.map((door) => {
-      return (
-        <Door
-          value={door}
-          key={door.number}
-          onChange={(newDoor) => setDoors(updatePorts(doors, newDoor))}
-        />
-      );
-    });
-  }
-
-  return <div style={{ display: "flex" }}>{renderDoors()}</div>;
+  return (
+    <div>
+      <h1>Beginning of the game</h1>
+    </div>
+  );
 }
