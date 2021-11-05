e2fe993c46a75b8bf62262190f7be072f1a79e89
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-04 22:05:12 -0300

    ✨ NumberInput component implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,36 @@
 import Card from "../Components/Card/Card";
 import styles from "../styles/Form.module.css";
-import Link from "next/link"
+import Link from "next/link";
+import NumberInput from "../Components/NumberInput/NumberInput";
+import { useState } from "react";
 
 export default function Form() {
+  const [numberDoors, setNumberDoors] = useState(3);
+  const [giftDoors, setGiftDoors] = useState(1);
   return (
     <div className={styles.formulario}>
       <div>
         <Card bgColor="#c0392c">
           <h1>Monty Hall</h1>
         </Card>
-        <Card></Card>
+        <Card>
+          <NumberInput
+            text="Quantidade de portas?"
+            value={numberDoors}
+            onChange={(newAmount) => setNumberDoors(newAmount)}
+          />
+        </Card>
       </div>
       <div>
-        <Card></Card>
+        <Card>
+          <NumberInput
+            text="Quantidade de portas?"
+            value={giftDoors}
+            onChange={(newAmount) => setGiftDoors(newAmount)}
+          />
+        </Card>
         <Card bgColor="#28a085">
-          <Link href={`/game/4/2`} passHref>
+          <Link href={`/game/${numberDoors}/${giftDoors}`} passHref>
             <h2 className={styles.link}>Iniciar</h2>
           </Link>
         </Card>
